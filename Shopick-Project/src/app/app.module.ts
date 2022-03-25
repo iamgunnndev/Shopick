c77ef4b7afef8c6c6a5159e7a92fba9d7c320629
Budsakornsil <102343004+Budsakornsil@users.noreply.github.com>
2022-03-25 12:36:03 +0700

    login

--- Shopick-Project/src/app/app.module.ts
+++ Shopick-Project/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ShophomeComponent } from './shophome/shophome.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ShophomeComponent
+    ShophomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
