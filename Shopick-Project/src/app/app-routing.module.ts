e2c7ee6dde8153ad6ea74128eae786116f452731
iamgunnndev <102342004+iamgunnndev@users.noreply.github.com>
2022-03-30 23:34:15 +0700

    Fixed

--- Shopick-Project/src/app/app-routing.module.ts
+++ Shopick-Project/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DetailComponent } from './detail/detail.component';
 import { HomeComponent } from './home/home.component';
+import { LoginRegisterComponent } from './login-register/login-register.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
-  {path:'detail/:id',component:DetailComponent}
+  {path:'detail/:id',component:DetailComponent},
+  {path:'signin',component:LoginRegisterComponent}
 
 ];
 
--- Shopick-Project/src/app/app.module.ts
+++ Shopick-Project/src/app/app.module.ts
@@ -5,27 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ShophomeComponent } from './shophome/shophome.component';
-import { LoginComponent } from './login/login.component';
 import { DetailComponent } from './detail/detail.component';
 import { ReactiveFormsModule } from '@angular/forms';
-<<<<<<< Updated upstream
 import { GallaryDirective } from './gallary.directive';
 import { LoginRegisterComponent } from './login-register/login-register.component'; 
-=======
-import {GalleriaModule} from 'primeng/galleria';
-import { GallaryDirective } from './gallary.directive';
-import { RegisterComponent } from './register/register.component';
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ShophomeComponent,
-    LoginComponent,
     DetailComponent,
     GallaryDirective,
-    RegisterComponent,
     LoginRegisterComponent
   ],
   imports: [
