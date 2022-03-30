c9d18b32b884b188a928d7614a3c2bf55a0f26e0
Budsakornsil <102343004+Budsakornsil@users.noreply.github.com>
2022-03-30 22:44:51 +0700

    Login-register

--- Shopick-Project/src/app/app-routing.module.ts
+++ Shopick-Project/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'detail/:id',component:DetailComponent}
-  
+
 ];
 
 @NgModule({
--- Shopick-Project/src/app/login-register/login-register.component.spec.ts
+++ Shopick-Project/src/app/login-register/login-register.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import { LoginRegisterComponent } from './login-register.component';
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+describe('LoginRegisterComponent', () => {
+  let component: LoginRegisterComponent;
+  let fixture: ComponentFixture<LoginRegisterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginRegisterComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
+    fixture = TestBed.createComponent(LoginRegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
